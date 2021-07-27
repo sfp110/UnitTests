b299bd07390a7db7600b755ed3aa42a206df5d79
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-27 16:13:57 +0100

    Change the test names

--- UnitTestLession/TestProject1/UnitTest1.cs
+++ UnitTestLession/TestProject1/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace TestProject1
 
         // Test Case #1: Expected Greeting to match the time provided : Verification
         [Test]
-        public void GivenATimeOf3_Greeting_RetunrsGoodEvening()
+        public void GivenATimeOf3_Greeting_RetunrsGoToBed()
         {
             var time = 3;
             var expectedGreeting = "Go to bed!";
@@ -24,7 +24,7 @@ namespace TestProject1
         }
         [Test]
         // Test Case #2: Expected Greeting to match the time provided : Verification
-        public void GivenATimeOf12_Greeting_RetunrsGoodEvening()
+        public void GivenATimeOf12_Greeting_RetunrsGoodMorning()
         {
             var time = 12;
             var expectedGreeting = "Good Morning";
@@ -35,7 +35,7 @@ namespace TestProject1
         }
         [Test]
         // Test Case #3: Expected Greeting to match the time provided : Verification
-        public void GivenATimeOf16_Greeting_RetunrsGoodEvening()
+        public void GivenATimeOf16_Greeting_RetunrsGoodAfternoon()
         {
             var time = 16;
             var expectedGreeting = "Good Afternoon";
@@ -57,7 +57,7 @@ namespace TestProject1
         }
         [Test]
         // Test Case #5: Expected Greeting to match the time provided : Verification
-        public void GivenATimeOf25_Greeting_RetunrsGoodEvening()
+        public void GivenATimeOf25_Greeting_RetunrsError()
         {
             var time = 25;
             var expectedGreeting = "Error.";
