0f9b4e19cc5e2bdb5f3f9a090cb4d130ecb4df10
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-27 14:21:14 +0100

    Add a simplistic menu for the time of day.

--- UnitTestLession/CodeTest/Program.cs
+++ UnitTestLession/CodeTest/Program.cs
@@ -6,7 +6,42 @@ namespace CodeTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int menuNumber=0;
+
+            do
+            {
+                Console.WriteLine("What time is it?");
+                string input = Console.ReadLine();
+                int number;
+
+                if (Int32.TryParse(input, out number))
+                {
+                    Console.WriteLine(DoTheTime(number));
+                    menuNumber+=1;
+
+                }
+                else
+                    Console.WriteLine("Your date does not work. \n");
+            }
+            while (menuNumber == 0);
+            
+        }
+
+        private static string DoTheTime(int number)
+        {
+            string greeting;
+            if (number >= 5 && number <= 12)
+            {
+                greeting = "Good Morning";
+            }
+            else if (number >= 12 && number <= 18)
+            {
+                greeting = "Good Afternoon";
+            }
+            else
+                greeting = "Good Evening";
+
+            return greeting;
         }
     }
 }
